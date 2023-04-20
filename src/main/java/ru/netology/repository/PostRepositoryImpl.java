53a6f885c5eb8610d5bfed2b59eac4ad4fceaf1e
KseniyaGunbina94 <senyagunbina@yandex.ru>
2023-04-20 22:22:34 +0300

    Выполнение ДЗ1.2 di-java

--- src/main/java/ru/netology/repository/PostRepositoryImpl.java
+++ src/main/java/ru/netology/repository/PostRepositoryImpl.java
@@ -1,14 +1,12 @@
 package ru.netology.repository;
 
-import org.springframework.stereotype.Repository;
-import ru.netology.model.Post;
 
+import ru.netology.model.Post;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 // Stub
-@Repository
 public class PostRepositoryImpl implements PostRepository {
   private AtomicLong count = new AtomicLong(0);
   private ConcurrentHashMap<Long, Post> allPostsForSave = new ConcurrentHashMap<>();
